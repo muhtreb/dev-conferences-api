68369d0bde5726e781defa437f45e6eb6717bd12
Julien HUMBERT <julien.humbert_prestataire@groupebayard.com>
2023-06-09 09:39:23 +0200

    Fix CORS *

--- src/EventListener/CorsEventListener.php
+++ src/EventListener/CorsEventListener.php
@@ -29,7 +29,7 @@ class CorsEventListener implements EventSubscriberInterface
     {
         $response = $event->getResponse();
         if ($response) {
-            $response->headers->set('Access-Control-Allow-Origin', $this->websiteUrl);
+            $response->headers->set('Access-Control-Allow-Origin', '*');
             $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
             $response->headers->set('Access-Control-Allow-Headers', 'content-type');
             $response->headers->set('Access-Control-Allow-Credentials', 'true');
@@ -60,7 +60,7 @@ class CorsEventListener implements EventSubscriberInterface
         }
 
         $response = $event->getResponse();
-        $response->headers->set('Access-Control-Allow-Origin', $this->websiteUrl);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'content-type');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
