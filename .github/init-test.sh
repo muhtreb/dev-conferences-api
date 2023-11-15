ad1080faad5dd792b69a5a67e3e4a584d732c47d
Julien HUMBERT <julien.humbert-prestataire@laposte.fr>
2023-11-15 11:47:38 +0100

    Fix init tests

--- .github/init-test.sh
+++ .github/init-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bin/console do:da:cr --env=test
-bin/console do:sc:up --force --env=test
+bin/console do:da:cr --env=test --quiet
+bin/console do:sc:up --force --env=test --complete
 bin/console do:fi:lo --env=test --no-interaction
