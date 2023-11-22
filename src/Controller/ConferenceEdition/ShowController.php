96b712661944593d81a4a6bdbd1e60b06a555dfc
Julien HUMBERT <julien.humbert-prestataire@laposte.fr>
2023-11-22 14:45:30 +0100

    Fix show edition

--- src/Controller/ConferenceEdition/ShowController.php
+++ src/Controller/ConferenceEdition/ShowController.php
@@ -5,6 +5,7 @@ namespace App\Controller\ConferenceEdition;
 use App\Entity\ConferenceEdition;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
@@ -17,9 +18,14 @@ class ShowController extends AbstractController
     )]
     public function __invoke(
         ConferenceEdition $conferenceEdition,
+        Request $request,
         NormalizerInterface $serializer
     ): JsonResponse
     {
-        return new JsonResponse($serializer->normalize($conferenceEdition));
+        return new JsonResponse($serializer->normalize($conferenceEdition, null, [
+            'withConference' => $request->query->getBoolean('withConference', true),
+            'withTalks' => $request->query->getBoolean('withTalks', true),
+            'withPlaylists' => $request->query->getBoolean('withPlaylists', true),
+        ]));
     }
 }
